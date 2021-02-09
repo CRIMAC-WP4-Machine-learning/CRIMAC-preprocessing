5c62673a9ef05b430db1ad15e4c4cf862c6d66af
Ibrahim Umar <ibrahim.umar@hi.no>
2021-02-09 01:05:11 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 var=${MODE:-script}
+master=${MASTER_HOST:localhost}
 
 # Either run master, worker, or the script
 echo "Mode is $var"
+echo "Scheduler address is $master"
 
 if [[ $var == "master" ]]; then
     dask-scheduler
 fi
 if [[ $var == "worker" ]]; then
-    dask-worker crimac-master:8786
+    dask-worker $master:8786
 fi
 if [[ $var == "script" ]]; then
   time python CRIMAC_preprocess.py
