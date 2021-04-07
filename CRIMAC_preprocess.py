839b9d07082d30dae356e204c8a12bbddf202852
Ibrahim Umar <ibrahim.umar@hi.no>
2021-04-07 11:23:10 +0200

    Re-organize license text

--- CRIMAC_preprocess.py
+++ CRIMAC_preprocess.py
@@ -1,11 +1,29 @@
 # -*- coding: utf-8 -*-
 """
-Reads raw file and convert it into a grid-ed format.
-Right now it can handle different range sizes between channels.
-Has the ability to save the resulting grid into netcdf or zarr.
+CRIMAC Master Preprocessing Script
 
-Copyright 2020, Ibrahim Umar, Nils Olav Handegard, Alba Ordonez, Rune Øyerhamn.
-Licensed under LGPL3.
+Reads EK60/EK8 raw files and convert it into a grid-ed format. Currently it
+can handle different range sizes between channels data. This script
+also has the ability to save the resulting grid into NetCDF or ZARR formatted
+files.
+
+Copyright (C) 2020, Ibrahim Umar, Nils Olav Handegard, Alba Ordonez, Rune
+Øyerhamn, and The Institute of Marine Research, Norway.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program; if not, write to the Free Software Foundation,
+Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
 """
 
 
